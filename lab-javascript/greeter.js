f82079eda7210445c8d966a6300d8cdebc6610ac
Michelle Steele <octoberclub@gmail.com>
2015-11-05 12:54:35 +0000

    Exercises for lab-javascript
    Completed the exercises

--- lab-javascript/greeter.js
+++ lab-javascript/greeter.js
@@ -1,3 +1,28 @@
+var Student = (function () {
+    function Student(firstname, middleinitial, lastname) {
+        this.firstname = firstname;
+        this.middleinitial = middleinitial;
+        this.lastname = lastname;
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
+    return Student;
+})();
 function greeter(person) {
-    return "Hello, " + person.firstname + " " + person.lastname + "\n\t\tthis is valid";
+    return "Hello, " + person.firstname + " " + person.lastname;
+}
+var user = new Student("Michelle", "Babe", "Steele");
+var user1 = new Student("Martyn", "Metal", "Osbourne");
+var user2 = new Student("Declan", "smiley", "Ward");
+var _a = printStudents(user, user1, user2), michelle = _a[0], martyn = _a[1], declan = _a[2];
+printStudents(michelle);
+function printStudents() {
+    var listOfStudents = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        listOfStudents[_i - 0] = arguments[_i];
+    }
+    for (var _a = 0; _a < listOfStudents.length; _a++) {
+        var v = listOfStudents[_a];
+        document.body.innerHTML += greeter(v) + "</br>";
+    }
+    return listOfStudents;
 }
--- lab-javascript/greeter.ts
+++ lab-javascript/greeter.ts
@@ -1,5 +1,30 @@
+class Student {
+	fullname:string;
+	constructor(public firstname, public middleinitial, public lastname){
+		this.fullname=firstname + " " + middleinitial + " " + lastname;
+	}
+}
+
+interface Person {
+	firstname:string;
+	lastname:string;
+}
 
 function greeter(person : Person) {
-		return `Hello, ${person.firstname} ${person.lastname}
-		this is valid`;
+		return `Hello, ${person.firstname} ${person.lastname}`;
 	}
+
+var user =new Student("Michelle", "Babe", "Steele");
+var user1=new Student("Martyn","Metal","Osbourne");
+var user2 = new Student("Declan","smiley","Ward");
+
+var [michelle, martyn, declan] = printStudents(user, user1, user2);
+
+printStudents(michelle);
+
+function printStudents(...listOfStudents: Student[]) {
+	for(var v of listOfStudents){
+		document.body.innerHTML += greeter(v) + "</br>";
+	}
+	return listOfStudents;
+}
